124e84f6c6cb2b0d9747cef17770ccb1d7a6f850
niklas <niklas.abe@hhu.de>
2026-09-21 11:10:20 +0200

    --no-edit

--- VisH/Model/GeneralUtils/Logger.cs
+++ VisH/Model/GeneralUtils/Logger.cs
@@ -4,12 +4,20 @@ namespace VisH.Model.GeneralUtils;
 
 public class Logger
 {
-    private static string _logfilePath =
-        "C:\\Users\\nikla\\OneDrive - Heinrich-Heine-Universitat Dusseldorf\\Dokumente\\Rechnungen\\log.txt";
+    private static string LogfilePath =>
+        Path.Combine(Config.UserDataDirectory, "log.txt");
 
     public static void Info(string message)
     {
         Console.WriteLine(message);
-        File.AppendAllText(_logfilePath, message);
+        try
+        {
+            Directory.CreateDirectory(Config.UserDataDirectory);
+            File.AppendAllText(LogfilePath, message + Environment.NewLine);
+        }
+        catch
+        {
+            // Ignore logging failures
+        }
     }
 }
